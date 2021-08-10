e1d95c8449fa1df2eca3dd57abcc299c5e6a2687
matkaas <mathias.kaas@outlook.com>
2021-08-10 17:22:17 +0200

    Update copyright notice

--- src/dk/dtu/compute/mavis/server/Server.java
+++ src/dk/dtu/compute/mavis/server/Server.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017-2020 The Technical University of Denmark
+ * Copyright 2017-2021 The Technical University of Denmark
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
