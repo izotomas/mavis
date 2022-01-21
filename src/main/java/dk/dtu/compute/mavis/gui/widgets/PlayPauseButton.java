a04ca5a9d22636bfa99b961a8b7a6a25dbb7ff4f
Tomas Izo <tomas@izo.sk>
2022-01-21 11:48:53 +0100

    year 2022

--- src/main/java/dk/dtu/compute/mavis/gui/widgets/PlayPauseButton.java
+++ src/main/java/dk/dtu/compute/mavis/gui/widgets/PlayPauseButton.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2017-2021 The Technical University of Denmark
+ * Copyright (C) 2017-2022 The Technical University of Denmark
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
