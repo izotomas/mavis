d7f1283483b61460400bdf062fb64f78937d9052
Tomas Izo <tomas@izo.sk>
2022-03-12 09:09:13 +0100

    more tests

--- src/test/java/dk/dtu/compute/cdl/validation/OperatorProviderTest.java
+++ src/test/java/dk/dtu/compute/cdl/validation/OperatorProviderTest.java
@@ -3,6 +3,7 @@ package dk.dtu.compute.cdl.validation;
 import static org.assertj.core.api.Assertions.*;
 import java.lang.reflect.InvocationTargetException;
 import java.util.stream.Stream;
+import org.javatuples.Pair;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -25,9 +26,20 @@ public class OperatorProviderTest {
   }
 
   private static Stream<Arguments> provideArgsForEvaluateTest() {
-    return Stream.of(Arguments.of("IS", (Integer) 4, (Integer) 5, false),
+    return Stream.of(
+        Arguments.of("OVERLAPS WITH",
+            new Pair<Pair<Integer, Integer>, Pair<Integer, Integer>>(
+                new Pair<Integer, Integer>(1, 1), new Pair<Integer, Integer>(1, 2)),
+            new Pair<Pair<Integer, Integer>, Pair<Integer, Integer>>(
+                new Pair<Integer, Integer>(1, 1), new Pair<Integer, Integer>(1, 2)),
+            true),
+        Arguments.of("IS", (Integer) 4, (Integer) 5, false),
         Arguments.of("IS", (Integer) 8, (Integer) 8, true),
         Arguments.of("IS NOT", (Integer) 9, (Integer) 9, false),
+        Arguments.of("IS LESS THAN", (Integer) 5, (Integer) 6, true),
+        Arguments.of("IS NOT LESS THAN", (Integer) 5, (Integer) 6, false),
+        Arguments.of("IS NOT MORE THAN", (Integer) 10, (Integer) 1, false),
+        Arguments.of("IS MORE THAN", (Integer) 122, (Integer) 0, true),
         Arguments.of("IS", "NoOp", "Push(E,E)", false), Arguments.of("IS", "NoOp", "NoOp", true),
         Arguments.of("IS NOT", "NoOp", "NoOp", false),
         Arguments.of("IS NOT", "NoOp", "Move(E)", true));
