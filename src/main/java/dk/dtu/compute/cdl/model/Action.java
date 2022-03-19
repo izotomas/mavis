aa808401826b4a5788f72a698c0ff3d55dffd1a3
Tomas Izo <tomas@izo.sk>
2022-03-19 11:36:44 +0100

    add constraint tests

--- src/main/java/dk/dtu/compute/cdl/model/Action.java
+++ src/main/java/dk/dtu/compute/cdl/model/Action.java
@@ -18,12 +18,39 @@ package dk.dtu.compute.cdl.model;
 import org.javatuples.*;
 
 public class Action {
+  /**
+   * origin coordinates (x,y)
+   */
   public final Pair<Integer, Integer> origin;
+  /**
+   * destination coordinates (x,y)
+   */
   public final Pair<Integer, Integer> destination;
+
+  /**
+   * timestep
+   */
   public final Integer time;
+
+  /**
+   * action name
+   */
   public final String name;
+
+  /**
+   * agent (id) performing the action
+   */
   public final Integer agent;
 
+  /**
+   * Action model used as context for constraint validation
+   * 
+   * @param origin orgin coodinates
+   * @param destination detination coordinates
+   * @param time time when action is being executed/requested
+   * @param name name of the action
+   * @param agent agent id attempting the action
+   */
   public Action(Pair<Integer, Integer> origin, Pair<Integer, Integer> destination, Integer time,
       String name, Integer agent) {
     this.origin = origin;
@@ -31,5 +58,21 @@ public class Action {
     this.time = time;
     this.name = name;
     this.agent = agent;
+  }
+
+  /**
+   * Constructor only for testing purpose
+   * 
+   * @param origX origin X coordinate
+   * @param origY origin Y coordinate
+   * @param destX destination X coordinate
+   * @param destY destination Y coordinate
+   */
+  protected Action(int origX, int origY, int destX, int destY) {
+    this.origin = new Pair<>(origX, origY);
+    this.destination = new Pair<>(destX, destY);
+    this.time = 0;
+    this.name = null;
+    this.agent = 0;
   }
 }
---
+++ src/test/java/dk/dtu/compute/cdl/model/ConstraintTest.java
@@ -0,0 +1,104 @@
+package dk.dtu.compute.cdl.model;
+
+import dk.dtu.compute.cdl.services.ConstraintParser;
+import static org.assertj.core.api.Assertions.*;
+import java.util.stream.Stream;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import dk.dtu.compute.cdl.errors.StatementParsingException;
+
+public class ConstraintTest {
+  private final static ConstraintParser PARSER = new ConstraintParser();
+
+  @Test
+  public void followingConstraintTriggersTest()
+      throws IllegalStateException, StatementParsingException {
+    // arrange
+    var followingConstraint = "ACTION a IS BLOCKED BY ACTION b IF a.dest IS b.origin";
+    var sut = PARSER.Parse(followingConstraint).withRequestingContext(new Action(1, 0, 1, 1))
+        .withRestrictingContext(new Action(1, 1, 1, 2)).build();
+
+
+    // act
+    var actual = sut.evaluate();
+
+    // assert
+    assertThat(actual).isTrue();
+  }
+
+  @Test
+  public void followingConstraintNotTriggersTest()
+      throws IllegalStateException, StatementParsingException {
+    // arrange
+    var followingConstraint = "ACTION a IS BLOCKED BY ACTION b IF a.dest IS b.origin";
+    var sut = PARSER.Parse(followingConstraint).withRequestingContext(new Action(1, 0, 1, 0))
+        .withRestrictingContext(new Action(1, 1, 1, 2)).build();
+
+
+    // act
+    var actual = sut.evaluate();
+
+    // assert
+    assertThat(actual).isFalse();
+  }
+
+
+  @ParameterizedTest
+  @MethodSource("provideActionsWithVertexOrFollowingConflict")
+  public void followingAndVertexConstraintTriggersTest(Action requesting, Action restricting)
+      throws IllegalStateException, StatementParsingException {
+    // arrange
+    var followingAndVertexConstraint =
+        "ACTION a IS BLOCKED BY ACTION b IF a.dest IS b.origin OR a.dest IS b.dest";
+    var sut = PARSER.Parse(followingAndVertexConstraint).withRequestingContext(requesting)
+        .withRestrictingContext(restricting).build();
+
+    // act
+    var actual = sut.evaluate();
+
+    // assert
+    assertThat(actual).isTrue();
+  }
+
+  private static Stream<Arguments> provideActionsWithVertexOrFollowingConflict() {
+    return Stream.of(Arguments.of(new Action(1, 0, 1, 1), new Action(1, 1, 1, 2)),
+        Arguments.of(new Action(1, 0, 1, 1), new Action(1, 0, 1, 1)));
+  }
+
+  @ParameterizedTest
+  @MethodSource("provideSwappingConstraintDefinitions")
+  public void swappingConstraintTriggersTest(String swappingConstraint)
+      throws IllegalStateException, StatementParsingException {
+    // arrange
+    var sut = PARSER.Parse(swappingConstraint).withRequestingContext(new Action(1, 1, 1, 2))
+        .withRestrictingContext(new Action(1, 2, 1, 1)).build();
+
+    // act
+    var actual = sut.evaluate();
+
+    // assert
+    assertThat(actual).isTrue();
+  }
+
+  @ParameterizedTest
+  @MethodSource("provideSwappingConstraintDefinitions")
+  public void swappingConstraintNotTriggersTest(String swappingConstraint)
+      throws IllegalStateException, StatementParsingException {
+    // arrange
+    var sut = PARSER.Parse(swappingConstraint).withRequestingContext(new Action(1, 0, 2, 0))
+        .withRestrictingContext(new Action(1, 2, 1, 1)).build();
+
+    // act
+    var actual = sut.evaluate();
+
+    // assert
+    assertThat(actual).isFalse();
+  }
+
+  private static Stream<Arguments> provideSwappingConstraintDefinitions() {
+    return Stream.of(Arguments.of("ACTION a IS BLOCKED BY ACTION b IF a.edge OVERLAPS WITH b.edge"),
+        Arguments.of("ACTION a IS BLOCKED BY ACTION b IF a.orig IS b.dest OR a.dest IS b.orig"));
+  }
+}
